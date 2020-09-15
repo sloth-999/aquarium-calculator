0ed8fb4f0dc00043e940666a78df740950d54906
sloth <42832072+sloth-999@users.noreply.github.com>
2020-09-15 12:17:51 +1000

    Add files via upload

--- aquarium.js
+++ aquarium.js
@@ -1,22 +1,32 @@
 window.onload=function() {
     lengthObj = document.getElementById('txtLength');
+    heightObj = document.getElementById('txtHeight');
     widthObj = document.getElementById('txtWidth');
-    costObj = document.getElementById('tdcost');
+	costObj = document.getElementById('tdCost');
     document.getElementById('btnReset').onclick = resetInputs;
-    document.getElementById('btnCalc').onclick = calccost;
+    document.getElementById('btnCalcCost').onclick = calcCost;
 }
 function resetInputs() {
     lengthObj.value = '';
+    heightObj.value = '';
     widthObj.value = '';
-    costObj.innerHTML = '';
+	costObj.innerHTML = '';	
 }
-function calccost() {
+function calcCost() {
     var length = new Number(lengthObj.value);
-    var width = new Number(widthObj.value);
+    var height = new Number(heightObj.value);
+	var width = new Number(widthObj.value);
     costObj.innerHTML = '';
-    if(isNaN(length) || isNaN(wdith)) {
-        alert('Invalid length or width');
+    if(isNaN(length) || isNaN(height) || isNaN(width)) {
+        alert('Invalid length or height or width');
         return;
     }
-    costObj.innerHTML = length*Math.pow(width,2)/800;
+
+
+    if (height >24.9){
+        costObj.innerHTML = "$" + (2*(length*height+width*height+length*width)*0.1+(height*4+length*2+width*2)*0.1+ 2*(length*height+width*height+length*width)*0.01)*1.1
+    } else {
+        costObj.innerHTML = "$" + (2*(length*height+width*height+length*width)*0.06+(height*4+length*2+width*2)*0.1+ 2*(length*height+width*height+length*width)*0.01)*1.1
+    }
+
 }
